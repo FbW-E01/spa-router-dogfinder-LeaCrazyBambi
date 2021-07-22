1f62f3fb94c89f0c51f4a8330f7632d8e1ba42b0
LeaCrazyBambi <lheynitz@gmail.com>
2021-07-22 16:06:01 +0200

    :D

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-
-import React from 'react';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Navigation from "./components/Navigation.jsx";
+import Whiskey from "./pages/Whiskey";
+import Hazel from "./pages/Hazel";
+import Tubby from "./pages/Tubby";
 
 function App() {
   return (
     <div className="App">
-      <h1>My React App</h1>
+      <h1>HELLOZ. WE HAVE DOGZ. CLICK ON THEM FOR MORE INFO.</h1>
+      <Navigation />
+      <Switch>
+        <Route path="/whiskey" component={Whiskey} />
+        <Route path="/hazel" component={Hazel} />
+        <Route path="/tubby" component={Tubby} />
+      </Switch>
     </div>
   );
 }
-
 
 export default App;
---
+++ src/pages/Tubby.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import "../components/Navigation.css"
+
+
+export default function Tubby() {
+    return <div className="bio-dogs">
+        <h2>Name: Tubby</h2>
+            Age: 4
+            <br></br>
+            Tubby is really stupid
+            <br></br>
+            Tubby does not like walks
+            <br></br>
+            Angelina hates Tubby
+    </div>
+}
